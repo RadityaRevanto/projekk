75dc5a51d41be07300f06c7bbe772122c94da35e
Raditya <ishariyanto598@gmail.com>
2022-10-30 16:12:55 +0700

    screen: [RR] add bottom profile

---
+++ lib/profile/profile_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:projekk/profile/component/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Body(),
+    );
+  }
+}
